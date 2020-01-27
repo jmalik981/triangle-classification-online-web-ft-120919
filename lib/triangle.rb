8891cfb46f40afb83eb06179ce911bd27960853e
Junaid Malik <jmalik981@gmail.com>
2020-01-27 07:09:46 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,13 @@
 class Triangle
-  # write code here
-end
+  attr_reader :a, :b, :c 
+  def initialize(a, b, c)
+    @a = a 
+    @b = b 
+    @c = c 
+  end
+  
+  def kind 
+    validate_triangle
+    if a == b && b == c 
+      :equilateral
+    elsif a == b || b == 
